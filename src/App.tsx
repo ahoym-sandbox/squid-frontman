a651fa6bbabbcafb52cd0d621fab49d0e4164ad1
Malcolm Ahoy <mahoy@ripple.com>
2021-11-02 18:42:31 -0400

    Execute oracle code immediately

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
-import { xrplClient, xrplClientTwo } from './XrplApiSandbox';
 
-// Can import and run TS scripts this way if so desired
-// import './XrplApiSandbox/scripts/sendXrp';
-// import './XrplApiSandbox/scripts/sendEscrow';
-// import './oracle';
-
-// Generate testnet wallets
-const generateWalletRequestOne = xrplClient.generateFaucetWallet();
-const generateWalletRequestTwo = xrplClientTwo.generateFaucetWallet();
+// Execute oracle code. Change BANK_ADDRESS
+import './oracle';
 
 function App() {
-  const [logs, setLogs] = useState<unknown[]>([]);
-
-  useEffect(() => {
-    generateWalletRequestOne.then((result) => {
-      setLogs((logState) => [
-        result,
-        'Created faucet wallet for Client 1',
-        ...logState,
-      ]);
-    });
-  }, []);
-
-  useEffect(() => {
-    generateWalletRequestTwo.then((result) => {
-      setLogs((logState) => [
-        result,
-        'Created faucet wallet for Client 2',
-        ...logState,
-      ]);
-    });
-  }, []);
-
-  useEffect(() => {
-    // After testnet wallet creations, send a 22 XRP payment
-    Promise.all([generateWalletRequestOne, generateWalletRequestTwo])
-      .then(() =>
-        xrplClient.sendPayment(22, xrplClientTwo.wallet()?.account.address!)
-      )
-      .then((result) => {
-        setLogs((logState) => [
-          result,
-          'Sent transaction from Wallet 1 to Wallet 2',
-          ...logState,
-        ]);
-      });
-  }, []);
+  const [logs] = useState<unknown[]>([]);
 
   return (
     <div className="App">
