41f44779a60dc79e8d90a39d9c096afa46108f9d
Malcolm Ahoy <mahoy@ripple.com>
2021-11-03 22:08:39 -0400

    Add drawn faces

--- src/features/PlayersContainer/index.tsx
+++ src/features/PlayersContainer/index.tsx
@@ -1,6 +1,13 @@
 import { usePlayersContext } from '../../contexts/usePlayersContext';
+import { Confused } from '../../faces/Confused';
+import { Happy } from '../../faces/Happy';
+import { Nooo } from '../../faces/Nooo';
+import { Oh } from '../../faces/Oh';
+import { Sad } from '../../faces/Sad';
 import { publishWinnerFulfillment } from '../../oracle';
 import './styles.css';
+
+const FACES = [Oh, Nooo, Confused, Happy, Sad];
 
 export function PlayersContainer() {
   const { players, removePlayer } = usePlayersContext();
@@ -9,6 +16,7 @@ export function PlayersContainer() {
   return (
     <div className="PlayerGrid">
       {Object.values(players).map((player) => {
+        const FaceComponent = FACES[Math.floor(Math.random() * FACES.length)];
         const disablePlayer = () => removePlayer(player.address);
 
         return (
@@ -29,6 +37,7 @@ export function PlayersContainer() {
           >
             <div className="PlayerCell-content">
               <p>{player.address}</p>
+              <FaceComponent />
             </div>
           </div>
         );
